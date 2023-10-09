1bdb84e7c537042fff968dc06588cf41e9007631
kylebarnham <kyle.barnham@cqumail.com>
2023-10-09 22:29:03 +1000

    added controller

---
+++ ProjectPartB/src/main/java/projectpartb/RentalMenuController.java
@@ -0,0 +1,59 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/javafx/FXMLController.java to edit this template
+ */
+package projectpartb;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.Button;
+/**
+ * FXML Controller class
+ *
+ * @author Kyle
+ */
+public class RentalMenuController implements Initializable {
+
+
+    @FXML
+    private Button creatButton;
+    @FXML
+    private Button viewButton;
+    @FXML
+    private Button findButton;
+    @FXML
+    private Button exitButton;
+    @FXML
+    private Button deleteButton;
+    /**
+     * Initializes the controller class.
+     */
+    @Override
+    public void initialize(URL url, ResourceBundle rb) {
+        // TODO
+    }    
+    
+    @FXML
+    private void createAction(ActionEvent event) {
+    }
+
+    @FXML
+    private void viewAction(ActionEvent event) {
+    }
+
+    @FXML
+    private void findAction(ActionEvent event) {
+    }
+
+    @FXML
+    private void exitAction(ActionEvent event) {
+    }
+
+    @FXML
+    private void deleteAction(ActionEvent event) {
+    }
+
+}
