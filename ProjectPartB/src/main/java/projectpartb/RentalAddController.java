146efddd6ae8be4e7df6a68dc6313c82417046d4
kylebarnham <kyle.barnham@cqumail.com>
2023-10-09 22:28:51 +1000

    added controller

---
+++ ProjectPartB/src/main/java/projectpartb/RentalAddController.java
@@ -0,0 +1,53 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/javafx/FXMLController.java to edit this template
+ */
+package projectpartb;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.Button;
+import javafx.scene.control.DatePicker;
+import javafx.scene.control.TextField;
+/**
+ * FXML Controller class
+ *
+ * @author Kyle
+ */
+public class RentalAddController implements Initializable {
+
+
+    @FXML
+    private Button backButton;
+    @FXML
+    private Button submitButton;
+    @FXML
+    private TextField RentalIDField;
+    @FXML
+    private TextField CustomerIDField;
+    @FXML
+    private TextField regoField;
+    @FXML
+    private DatePicker rentalStartDate;
+    @FXML
+    private DatePicker rentalEndDate;
+    /**
+     * Initializes the controller class.
+     */
+    @Override
+    public void initialize(URL url, ResourceBundle rb) {
+        // TODO
+    }    
+    
+    @FXML
+    private void backAction(ActionEvent event) {
+    }
+
+    @FXML
+    private void submitAction(ActionEvent event) {
+    }
+
+}
