1991b577b34f6703f370ddd46b829414001e4c02
kylebarnham <kyle.barnham@cqumail.com>
2023-10-09 22:28:08 +1000

    added controller

---
+++ ProjectPartB/src/main/java/projectpartb/CustomerMainController.java
@@ -0,0 +1,59 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/javafx/FXMLController.java to edit this template
+ */
+package projectpartb;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.Button;
+/**
+ * FXML Controller class
+ *
+ * @author Kyle
+ */
+public class CustomerMainController implements Initializable {
+
+
+    @FXML
+    private Button createRentalButton;
+    @FXML
+    private Button vehicleButton;
+    @FXML
+    private Button editButton;
+    @FXML
+    private Button exitButton;
+    @FXML
+    private Button checkRentalButton;
+    /**
+     * Initializes the controller class.
+     */
+    @Override
+    public void initialize(URL url, ResourceBundle rb) {
+        // TODO
+    }    
+    
+    @FXML
+    private void createRentalAction(ActionEvent event) {
+    }
+
+    @FXML
+    private void seeVehiclesAction(ActionEvent event) {
+    }
+
+    @FXML
+    private void editAction(ActionEvent event) {
+    }
+
+    @FXML
+    private void exitAction(ActionEvent event) {
+    }
+
+    @FXML
+    private void checkRentalAction(ActionEvent event) {
+    }
+
+}
