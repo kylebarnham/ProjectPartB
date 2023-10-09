b3879f342da6a20a5623a1d0bdfe794074531b78
kylebarnham <kyle.barnham@cqumail.com>
2023-10-09 22:30:01 +1000

    added controller

---
+++ ProjectPartB/src/main/java/projectpartb/VehicleFindController.java
@@ -0,0 +1,53 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/javafx/FXMLController.java to edit this template
+ */
+package projectpartb;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.Button;
+import javafx.scene.control.TextArea;
+import javafx.scene.control.TextField;
+/**
+ * FXML Controller class
+ *
+ * @author Kyle
+ */
+public class VehicleFindController implements Initializable {
+
+
+    @FXML
+    private Button backButton;
+    @FXML
+    private Button editButton;
+    @FXML
+    private TextField regoField;
+    @FXML
+    private TextArea textArea;
+    @FXML
+    private Button submitButton1;
+    /**
+     * Initializes the controller class.
+     */
+    @Override
+    public void initialize(URL url, ResourceBundle rb) {
+        // TODO
+    }    
+    
+    @FXML
+    private void backAction(ActionEvent event) {
+    }
+
+    @FXML
+    private void editAction(ActionEvent event) {
+    }
+
+    @FXML
+    private void submitAction(ActionEvent event) {
+    }
+
+}
