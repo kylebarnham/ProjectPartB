aecab95639e8d6dc885368d04bc73e843f0f702e
kylebarnham <kyle.barnham@cqumail.com>
2023-10-09 22:29:56 +1000

    added controller

---
+++ ProjectPartB/src/main/java/projectpartb/VehicleAddController.java
@@ -0,0 +1,119 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/javafx/FXMLController.java to edit this template
+ */
+package projectpartb;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.Button;
+import javafx.scene.control.MenuButton;
+import javafx.scene.control.TextField;
+/**
+ * FXML Controller class
+ *
+ * @author Kyle
+ */
+public class VehicleAddController implements Initializable {
+
+
+    @FXML
+    private TextField odoFieldCar;
+    @FXML
+    private MenuButton availableMenuCar;
+    @FXML
+    private TextField colourFieldCar;
+    @FXML
+    private TextField modelFieldCar;
+    @FXML
+    private TextField regoFieldCar;
+    @FXML
+    private TextField typeFieldCar;
+    @FXML
+    private TextField passengerFieldCar;
+    @FXML
+    private TextField yearFieldCar;
+    @FXML
+    private TextField costFieldCar;
+    @FXML
+    private TextField manufacturerFieldCar;
+    @FXML
+    private Button submitButtonCar;
+    @FXML
+    private Button backButtonCar;
+    @FXML
+    private TextField odoFieldVan;
+    @FXML
+    private MenuButton availableMenuVan;
+    @FXML
+    private TextField colourFieldVan;
+    @FXML
+    private TextField manufacturerFieldVan;
+    @FXML
+    private TextField regoFieldVan;
+    @FXML
+    private TextField passengerFieldVan;
+    @FXML
+    private TextField yearFieldVan;
+    @FXML
+    private TextField costFieldVan;
+    @FXML
+    private TextField modelFieldVan;
+    @FXML
+    private Button submitButtonVan;
+    @FXML
+    private Button backButtonVan;
+    @FXML
+    private MenuButton wheelchairMenu;
+    @FXML
+    private TextField odoFieldBus;
+    @FXML
+    private MenuButton availableMenuBus;
+    @FXML
+    private TextField colourFieldBus;
+    @FXML
+    private TextField modelFieldBus;
+    @FXML
+    private TextField regoFieldBus;
+    @FXML
+    private TextField passengerFieldBus;
+    @FXML
+    private TextField yearFieldBus;
+    @FXML
+    private TextField costFieldBus;
+    @FXML
+    private TextField manufacturerFieldBus;
+    @FXML
+    private Button submitButtonBus;
+    @FXML
+    private Button backButtonBus;
+    @FXML
+    private TextField typeFieldBus;
+    /**
+     * Initializes the controller class.
+     */
+    @Override
+    public void initialize(URL url, ResourceBundle rb) {
+        // TODO
+    }    
+    
+    @FXML
+    private void submitCar(ActionEvent event) {
+    }
+
+    @FXML
+    private void backAction(ActionEvent event) {
+    }
+
+    @FXML
+    private void submitVan(ActionEvent event) {
+    }
+
+    @FXML
+    private void submitBus(ActionEvent event) {
+    }
+
+}
