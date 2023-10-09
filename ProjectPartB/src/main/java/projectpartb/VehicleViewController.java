67a6069b95e733b32a64239d4a3f5f6f9d99958d
kylebarnham <kyle.barnham@cqumail.com>
2023-10-09 22:30:29 +1000

    added controller

---
+++ ProjectPartB/src/main/java/projectpartb/VehicleViewController.java
@@ -0,0 +1,44 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/javafx/FXMLController.java to edit this template
+ */
+package projectpartb;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.Button;
+import javafx.scene.control.TableColumn;
+/**
+ * FXML Controller class
+ *
+ * @author Kyle
+ */
+public class VehicleViewController implements Initializable {
+
+
+    @FXML
+    private Button backButton;
+    @FXML
+    private TableColumn<?, ?> idColumn;
+    @FXML
+    private TableColumn<?, ?> nameColumn;
+    @FXML
+    private TableColumn<?, ?> phoneColumn;
+    @FXML
+    private TableColumn<?, ?> emailColumn;
+    /**
+     * Initializes the controller class.
+     */
+    @Override
+    public void initialize(URL url, ResourceBundle rb) {
+        // TODO
+    }    
+    
+    @FXML
+    private void backAction(ActionEvent event) {
+    }
+
+}
